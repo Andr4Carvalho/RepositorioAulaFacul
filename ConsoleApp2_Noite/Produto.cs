5f53b3bc713525fd77e8b18e0d3c1d05d62f289a
Andr4Carvalho <andrecarvalhoprado@gmail.com>
2023-03-10 20:07:58 -0300

    Metodos set

--- ConsoleApp2_Noite/Produto.cs
+++ ConsoleApp2_Noite/Produto.cs
@@ -23,15 +23,29 @@ namespace ConsoleApp2_Noite
         {
             return Descricao;
         }
+        public void setDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
 
         public decimal getValor()
         {
             return Valor;
         }
 
+        public void setValor(decimal valor)
+        {
+            Valor = valor;
+        }
+
         public int getId()
         {
             return Id;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
         }
     }
 }
--- ConsoleApp2_Noite/Program.cs
+++ ConsoleApp2_Noite/Program.cs
@@ -1,7 +1,10 @@
 ﻿using ConsoleApp2_Noite;
 
-Console.WriteLine("Hello, World!");
-
 Produto p1 = new Produto(1, 10.90M, "Cervejinha");
 
-Console.WriteLine(p1.getDescricao() + " " + p1.getId());
+Console.WriteLine(p1.getDescricao() + " " + p1.getId() + "\n");
+
+p1.setValor(11.85M);
+p1.setDescricao("Cerveja litrão Skol");
+
+Console.WriteLine(p1.getDescricao() + ": " + p1.getValor());
