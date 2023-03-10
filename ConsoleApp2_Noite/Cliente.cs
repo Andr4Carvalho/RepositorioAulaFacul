7b786b89e771db858abb4796acae9865cf7b738f
Andr4Carvalho <andrecarvalhoprado@gmail.com>
2023-03-10 19:49:05 -0300

    Commit inicial

---
+++ ConsoleApp2_Noite/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_Noite
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+        public Cliente(int id, string nome, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public string GetCpf() {
+            return Cpf;
+        }
+    }
+}
--- ConsoleApp2_Noite/Program.cs
+++ ConsoleApp2_Noite/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleApp2_Noite;
+
 Console.WriteLine("Hello, World!");
+
+List<Cliente> clientes = new List<Cliente>();
+
+for(int i = 0; i < 5000; i++)
+{
+    string nome = "Cliente {i}";
+}
